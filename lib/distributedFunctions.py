Fixed sign issues of shear function

lib/distributedFunctions.py
@@ -295,14 +295,15 @@ def shearConditions( y , u , nu , U_inf=None ):
 
     # Calculate shear velocity
     dy = np.gradient( y )[0]
-    #print("dy:\t{x:.3e}".format(x=dy))
+    print("dy:\t{x:.3e}".format(x=dy))
     du = np.gradient( u )[0]
-    #print("du:\t{x:.3e}".format(x=du))
-    #print("tau_w:\t{x:.3f}".format(x=np.abs( du / dy )))
-    u_tau = np.sqrt( nu * np.abs( du / dy ) )
+    print("du:\t{x:.3e}".format(x=du))
+    tau_w = nu * ( du / dy )
+    print("tau_w:\t{x:.3f}".format(x=np.abs( du / dy )))
+    u_tau = np.sqrt( np.abs( tau_w ) ) * ( tau_w / np.abs( tau_w ) )
     
     # Skin friction coefficient
-    C_f = 2 * ( ( u_tau / U_inf ) ** 2 )
+    C_f = 2 * ( ( u_tau / U_inf ) ** 2 ) * ( tau_w / np.abs( tau_w ) )
 
     return u_tau , C_f 
 